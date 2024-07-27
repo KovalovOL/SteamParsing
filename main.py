e69af7ebfcc7259a8ef6f3c499687cf24e25b5e9
KIKIMORA <sasha.kovalov2008@gmail.com>
2024-07-28 00:37:11 +0300

    Refactoring update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import flet as ft
 from parsing import *
 
 def main(page: ft.Page):
-    
     page.title = "Your steam stats"
     page.window.width = 1100
     page.window.height = 700
@@ -12,264 +11,250 @@ def main(page: ft.Page):
 
     def navigate_to_page1(e):
         page.go("page1")
-        input_field.value = ""
+        InputField.value = ""
 
     def navigate_to_page2(e):
         page.go("page2")
         
+    def page1_view():
+        return ft.View(
+            "page1", [
+                ft.Container(
+                    content=MainColumn,
+                    alignment=ft.alignment.center
+                )
+            ]
+        )
+    
     def page2_view():
-        steam_link = input_field.value
-
-        user_avatar = ft.Stack(
-            controls=[
-            ft.Container(content=ft.Image(src=get_avatar(steam_link), width=230, height=230), width=230, height=230),
-            ft.Container(content=ft.Image(src=get_avatar_border(steam_link), width=230, height=230), width=230, height=230)
-        ])
+        steam_link = InputField.value
         
-        user_name = ft.Text(
-            value=get_name(steam_link),
-            size = 40,
-        )
-
-        user_level_text = ft.Text(
-            value =  "Level",
-            size = 30
-        )
-
-        user_level = ft.Text(
-            value=  get_lvl(steam_link),
-            size = 30,
-            color = set_level_color(get_lvl(steam_link))
-        )
-        
-        if get_is_online(steam_link) == "Currently Offline":
-            online_color = "#78786D"
-        elif get_is_online(steam_link) == "Currently Online":
-             online_color = "#5C9DC6"
-        else:
-            online_color = "#65B144"
-            
-        user_online_status = ft.Text(
-            value = get_is_online(steam_link),
-            size = 30,
-            color = online_color
-        )
-
-        online_status_list = [user_online_status]
-        if get_is_online(steam_link) == "Currently In-Game":
-            user_ingame_status = ft.Text(
-                value= get_is_online(steam_link, return_what_game_playing=True),
-                size= 30,
-                color = online_color
+        #Set online status
+        online_status_list = [
+            ft.Text(
+                value = get_is_online(steam_link),
+                size = 30,
+                color = set_online_color(steam_link)
             )
-            online_status_list.append(user_ingame_status)
+        ]
+
+        if get_is_online(steam_link) == "Currently In-Game":
+            online_status_list.append(
+                ft.Text(
+                    value = get_is_online(steam_link, return_what_game_playing=True), 
+                    size= 30,
+                    color = set_online_color(steam_link)
+                )
+            )
 
 
-        online_status_column = ft.Column(
-            controls=online_status_list
-        )
+        Avatar = ft.Container(
+            content = ft.Stack(
+                controls=[
+                    #Avatar
+                    ft.Container(
+                        content=ft.Image(
+                            src=get_avatar(steam_link),
+                            width=230,
+                            height=230
+                        ),
+                    ),
 
-        level_row = ft.Row(
-            controls=[user_level_text, user_level]
-        )
+                    #Avatars border
+                    ft.Container(
+                        content=ft.Image(
+                            src=get_avatar_border(steam_link),
+                            width=230,
+                            height=230
+                        ),
+                    )
+                ]
+            )
+        ) 
 
-        c0 = ft.Container(
-            content = user_avatar
-        )
-
-        c1 = ft.Container(
-            content = user_name,
+        UserName = ft.Container(
+            content = ft.Text(
+                value = get_name(steam_link),
+                size = 40
+            ),
             padding = ft.Padding(left = 15, right= 0, top = 120, bottom= 0)
+        ) 
+            
+        LevelRow = ft.Row(
+            controls=[
+                #Level text
+                ft.Container(
+                    content=ft.Text(
+                        value =  "Level",
+                        size = 30
+                    ), 
+                    padding= ft.Padding(left = 0, right = 5, top = 105, bottom = 40)
+                ), 
+
+                #Level value
+                ft.Text(
+                    value =  get_lvl(steam_link),
+                    size = 30,
+                    color = set_level_color(get_lvl(steam_link))
+                )
+            ]
         )
 
-        c2 = ft.Container(
-            content=level_row,
-            padding= ft.Padding(left = 0, right = 5, top = 105, bottom = 40)
-        )
-
-        c_online = ft.Container(
-            content=online_status_column,
+        OnlineStatus = ft.Container(
+            content=ft.Column(
+                controls = online_status_list
+            ),
             padding = ft.Padding(left = 0, right= 0, top = 0, bottom= 165),
         )
-
-        username_row = ft.Row(
-        controls = [c0, c1, c2, c_online],
-        alignment = ft.MainAxisAlignment.START,
-        vertical_alignment = ft.CrossAxisAlignment.CENTER
-        )
-        #--------------------------------------------------------------------
-
-        hours_past_two_weeks = get_hours_past_two_weeks(steam_link)
-        if hours_past_two_weeks != "No information":
-            hours_past_two_weeks += " hours past 2 weeks "
         
-        past_two_weeks_text = ft.Text(
-            value=hours_past_two_weeks,
-            size = 30
+        UserRow = ft.Row(
+            alignment = ft.MainAxisAlignment.START,
+            vertical_alignment = ft.CrossAxisAlignment.CENTER,
+            controls =[Avatar, UserName, LevelRow, OnlineStatus]
         )
 
-        last_game_text = ft.Text(
-            value= "Last game played:",
-            size = 20
-        )
 
-        last_game_name = ft.Text(
-            value=get_last_game(steam_link, return_str = True),
-            size = 35
-        )
-
-        if get_last_game(steam_link) != "No information":
-            last_game_img = ft.Image(
-                src = get_last_game(steam_link, return_str=False),
-                width=276,
-                height=103,
-                fit=ft.ImageFit.CONTAIN
-            )
-
-            c6 = ft.Container(
-            content=last_game_img,
-            padding= ft.Padding(left = 20, right = 0, top = 0, bottom = 0)
-            )
-
-        c3 = ft.Container(
-            content=past_two_weeks_text,
+        HoursPastText = ft.Container(
+            content = ft.Text(
+                value=get_hours_past_two_weeks(steam_link),
+                size = 30
+            ),
             padding= ft.Padding(left = 20, right = 0, top = 0, bottom = 0)
         )
 
-        c4 = ft.Container(
-            content=last_game_text,
+        LastGameText = ft.Container(
+            content = ft.Text(
+                value= "Last game played:",
+                size = 20
+            ),
             padding= ft.Padding(left = 20, right = 0, top = 0, bottom = 0)
         )
 
-        c5 = ft.Container(
-            content=last_game_name,
+        LastGameName = ft.Container(
+            content=ft.Text(
+                value=get_last_game(steam_link, return_str = True),
+                size = 35
+            ),
             padding= ft.Padding(left = 20, right = 0, top = 25, bottom = 0)
         )
 
 
-        gamestats_column_list = [c3, c4, c5]
+        GameStatsColumnList = [HoursPastText, LastGameText, LastGameName]
         if get_last_game(steam_link, return_str=False) != "No information":
-            gamestats_column_list.append(c6)
+            GameStatsColumnList.append(
+                ft.Container(
+                    content = ft.Image(
+                        src = get_last_game(steam_link, return_str=False),
+                        width=276,
+                        height=103,
+                        fit=ft.ImageFit.CONTAIN
+                    ),
+                    padding= ft.Padding(left = 20, right = 0, top = 0, bottom = 0)
+                )
+            )
 
-        gamestats_column = ft.Column(
-            controls=gamestats_column_list,
+        GameStatsColumn = ft.Column(
+            controls=GameStatsColumnList,
             alignment = ft.MainAxisAlignment.START,
             horizontal_alignment=ft.CrossAxisAlignment.START,
         )
 
-        #--------------------------------------------------------------------
-
-        friends_count_text = ft.Text(
-            value=f"Friends count: {get_friends_count(steam_link)}",
-            size = 35
+        
+        FriendsCountText = ft.Container(
+            content=ft.Text(
+                value=f"Friends count: {get_friends_count(steam_link)}",
+                size = 35
+            ),
+            padding= ft.Padding(left = 50, right = 0, top = 0, bottom = 0)
         )
 
-        games_count_text = ft.Text(
-            value=f"Games count: {get_games_count(steam_link)}",
-            size = 35
+        GamesCountText = ft.Container(
+            content=ft.Text(
+                value=f"Games count: {get_games_count(steam_link)}",
+                size = 35
+            ),
+            padding= ft.Padding(left = 50, right = 0, top = 0, bottom = 0)
         )
 
-        back_button = ft.CupertinoButton(
+        BackButton = ft.Container(
+            content=ft.CupertinoButton(
             text = "Back",
             color = "black",
             bgcolor= "#A9A9A9",
             on_click= navigate_to_page1,
-            width= 250,
+            width= 200,
             height=50,
+            ),
+            padding= ft.Padding(left = 0, right = 0, top = 250, bottom = 0),
         )
 
-
-        c7 = ft.Container(
-            content=friends_count_text,
-            padding= ft.Padding(left = 50, right = 0, top = 0, bottom = 0)
-        )
-
-        c8 = ft.Container(
-            content=games_count_text,
-            padding= ft.Padding(left = 50, right = 0, top = 0, bottom = 0)
-        )
-
-        c9 = ft.Container(
-            content=back_button,
-            padding= ft.Padding(left = 100, right = 0, top = 250, bottom = 0)
-        )
-
-
-        accountstats_column = ft.Column(
-            controls= [c7, c8],
+        AccountStatsColumn = ft.Column(
+            controls= [FriendsCountText, GamesCountText],
             alignment = ft.MainAxisAlignment.START,
             horizontal_alignment=ft.CrossAxisAlignment.START    
         )
 
-        bottom_row = ft.Row(
-            controls=[gamestats_column, accountstats_column, c9]
+        BottomRow = ft.Row(
+            controls=[GameStatsColumn, AccountStatsColumn, BackButton]
         )
 
-        main_column = ft.Column(
-            controls=[username_row, bottom_row]
+        MainColumn = ft.Column(
+            controls=[UserRow , BottomRow]
         )
-
 
         return ft.View("page2",
         [
-        ft.Container(
-        content = main_column,
-        alignment=ft.alignment.center,
-        padding = 20)
-        ])
-    
-    def page1_view():
-        return ft.View("page1",
-        [
-        ft.Container(
-        content=column,
-        alignment=ft.alignment.center)
+            ft.Container(
+                content = MainColumn,
+                alignment=ft.alignment.center,
+                padding = 20
+            )
         ])
     
 
-    main_text = ft.Text(
-        value= "Enter your steam link",
-        size = 45
-    )
-
-    input_field = ft.TextField(
-        width = 600,
-        height = 50,
-        bgcolor="white",
-        color = "black"
-    )
-
-    start_button = ft.CupertinoButton(
-        text = "Press to start",
-        color = "black",
-        bgcolor= "white",
-        min_size=60,
-        alignment=ft.alignment.center,
-        on_click= navigate_to_page2,
-    )
+    
 
 
-    c1 = ft.Container(
-        content=main_text,
+   
+
+
+    MainText = ft.Container(
+        content = ft.Text(
+            value= "Enter your steam link",
+            size = 45
+        ),
         padding= ft.Padding(left = 0, right = 0, top = 50, bottom = 0)
     )
 
-    c2 = ft.Container(
-        content=input_field,
+    InputField = ft.TextField(
+            width = 600,
+            height = 50,
+            bgcolor="white",
+            color = "black"
+    )
+    
+    InputFieldContainer = ft.Container(
+        content = InputField,
         padding= ft.Padding(left = 0, right = 0, top = 170, bottom = 0)
     )
 
-    c3 = ft.Container(
-        content=start_button,
+    StartButton = ft.Container(
+        content = ft.CupertinoButton(
+            text = "Press to start",
+            color = "black",
+            bgcolor = "white",
+            min_size = 60,
+            alignment = ft.alignment.center,
+            on_click = navigate_to_page2,
+        ),
         padding= ft.Padding(left = 0, right = 0, top = 0, bottom = 0)
     )
 
-    column = ft.Column(
-        controls=[c1, c2, c3],
+    MainColumn = ft.Column(
+        controls=[MainText, InputFieldContainer, StartButton],
         alignment = ft.MainAxisAlignment.CENTER,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER)
     
-    #--------------------------------------------------------------------
     def route_change(route):
         page.views.clear()
         if page.route == 'page2':
--- parsing.py
+++ parsing.py
@@ -10,7 +10,7 @@ def main_soup(steam_link):
 def get_hours_past_two_weeks(steam_link):
     recentgame_quicklinks_recentgame_recentplaytime_div = main_soup(steam_link).find("div", class_ = "recentgame_quicklinks recentgame_recentplaytime")
     try:
-        hours_past_two_weeks = recentgame_quicklinks_recentgame_recentplaytime_div.find("div").text.split()[0]
+        hours_past_two_weeks = recentgame_quicklinks_recentgame_recentplaytime_div.find("div").text
     except AttributeError:
         hours_past_two_weeks = "No information"
     return hours_past_two_weeks
@@ -42,6 +42,18 @@ def get_last_game(steam_link, return_str = True):
     else:
         return game_img
 
+def get_is_online(steam_link, return_what_game_playing = False):
+    is_online = main_soup(steam_link).find("div", class_ = "profile_in_game_header").text
+
+    if is_online == "Currently In-Game" and return_what_game_playing == True:
+        profile_in_game_name_text = main_soup(steam_link).find("div", class_ = "profile_in_game_name").text.split()
+        game_name = ""
+        for i in profile_in_game_name_text:
+            game_name += i
+            game_name += " "
+        return game_name[:-1]
+    return is_online
+
 def set_level_color(level):
     level = int(level)
 
@@ -60,17 +72,14 @@ def set_level_color(level):
     elif level >= 80 and level < 90: return "#6B1A33"
     elif level >= 90 and level <= 99: return "#6B1A33"
 
-def get_is_online(steam_link, return_what_game_playing = False):
-    is_online = main_soup(steam_link).find("div", class_ = "profile_in_game_header").text
-
-    if is_online == "Currently In-Game" and return_what_game_playing == True:
-        profile_in_game_name_text = main_soup(steam_link).find("div", class_ = "profile_in_game_name").text.split()
-        game_name = ""
-        for i in profile_in_game_name_text:
-            game_name += i
-            game_name += " "
-        return game_name[:-1]
-    return is_online
+def set_online_color(steam_link):
+    if get_is_online(steam_link) == "Currently Offline":
+        online_color = "#78786D"
+    elif get_is_online(steam_link) == "Currently Online":
+        online_color = "#5C9DC6"
+    else:
+        online_color = "#65B144"
+    return online_color
     
 def get_friends_count(steam_link):
     profile_friend_links_profile_count_link_preview_ctn_responsive_groupfriends_element_div = main_soup(steam_link).find("div",
